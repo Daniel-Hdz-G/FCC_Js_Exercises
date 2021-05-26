6bff96473b0c3bfd587b0384abdf6d999622cbd0
Daniel Hernández <47311620+Daniel-Hdz-G@users.noreply.github.com>
2021-05-26 00:13:25 -0500

    Create HTMLSpecialSymbols.js

---
+++ HTMLSpecialSymbols.js
@@ -0,0 +1,14 @@
+function convertHTML(str) {
+  // We need to create an object with all the values that we need to replace
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  // Using a regex, replace characters with it's corresponding html entity
+  return str.replace(/([&<>\"'])/g, match => htmlEntities[match]);
+}
+
+console.log(convertHTML("Sixty > twelve"));
